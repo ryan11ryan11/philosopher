24b74eb2e33fd3d7f5b3ebc66963c47b4690c993
Junhwa Hong <junhhong@c2r7s6.42wolfsburg.de>
2024-07-05 14:36:07 +0200

    yosi

--- philosopher.h
+++ philosopher.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junhhong <junhhong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 14:33:42 by junhhong          #+#    #+#             */
+/*   Updated: 2024/07/05 14:35:57 by junhhong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILOSOPHER_H
 # define PHILOSOPHER_H
 # include <pthread.h>
@@ -16,15 +28,15 @@
 
 typedef struct s_args
 {
-	int	nop; // number of philosopher
-	int	ttd; // time to die
-	int	tte; // time to eat
-	int	tts; // time to sleep
-	int	not; // number_of_times_each_philosopher_must_eat
-	int	is_died;
-	int	all_full;
-	long	start_time;
-	pthread_t	*philo_group;
+	int					nop; // number of philosopher
+	int					ttd; // time to die
+	int					tte; // time to eat
+	int					tts; // time to sleep
+	int					not; // number_of_times_each_philosopher_must_eat
+	int					is_died;
+	int					all_full;
+	long				start_time;
+	pthread_t			*philo_group;
 	struct s_philo		*philo_struct;
 	pthread_mutex_t		*fork;
 	pthread_mutex_t		announce_lock;
@@ -32,41 +44,41 @@ typedef struct s_args
 
 typedef struct s_arginfo
 {
-	int	philo_num;
+	int		philo_num;
 	t_args	*args;
 }	t_arginfo;
 
 typedef struct s_philo
 {
-	int	philo_index;
-	long	last_eat;
-	int	meals;
-	int	finished;
+	int				philo_index;
+	long			last_eat;
+	int				meals;
+	int				finished;
 	pthread_mutex_t	*l_fork;
 	pthread_mutex_t	*r_fork;
-	t_args	*args;
+	t_args			*args;
 }	t_philo;
 
-int	ft_atoi(const char *nptr);
+int				ft_atoi(const char *nptr);
 
-long	get_time(void);
-int	announce(t_args *args, int philo_index, char *msg);
-char	*ft_strchr(const char *s, int c);
+long			get_time(void);
+int				announce(t_args *args, int philo_index, char *msg);
+char			*ft_strchr(const char *s, int c);
 
-pthread_t	*philo_maker(t_args *args);
+pthread_t		*philo_maker(t_args *args);
 pthread_mutex_t	*forkmaker(t_args *args);
 
-void	fork_release(t_args *args, int	philo_index);
-void	fork_take(t_args *args, int	philo_index);
-void	thinking(t_args *args, int philo_index);
-void	sleeping(t_args *args, int philo_index);
+void			fork_release(t_args *args, int philo_index);
+void			fork_take(t_args *args, int philo_index);
+void			thinking(t_args *args, int philo_index);
+void			sleeping(t_args *args, int philo_index);
 
-int	set_args(t_args *args, char *argv[], int argc);
-int	argument_check(int argc, char *argv[], t_args *args);
-void one_philo(t_args *args);
+int				set_args(t_args *args, char *argv[], int argc);
+int				argument_check(int argc, char *argv[], t_args *args);
+void			one_philo(t_args *args);
 
-long	get_time();
-void	*philo_action(void *arginfo);
-int	announce(t_args *args, int philo_index, char *msg);
+long			get_time(void);
+void			*philo_action(void *arginfo);
+int				announce(t_args *args, int philo_index, char *msg);
 
 #endif
